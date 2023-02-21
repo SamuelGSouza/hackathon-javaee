aa955f2f8f1fcfccdd83b1aae1dcaadb5e53202d
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-21 05:42:11 -0300

    chore: adiciona carga-inicial.sql

---
+++ src/main/resources/db/carga-inicial.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_usuario (nome, login, email, senha, data_nascimento, data_criacao)
+VALUES ('João da Silva', 'joao_silva', 'joão.silva@gmail.com', '$2a$10$aZf7xh6zf.2NPNNIuxGTzej3.YRVDSv9FE4hMUZYky0KhlkN0oZcq', '2000/01/01', '2023-02-21 10:30:00');
+
+INSERT INTO tb_usuario (nome, login, email, senha, data_nascimento, data_criacao)
+VALUES ('Maria Souza', 'maria_souza', 'maria.souza@outlook.com', '$2a$10$aZf7xh6zf.2NPNNIuxGTzej3.YRVDSv9FE4hMUZYky0KhlkN0oZcq', '2000/01/01', '2023-02-21 10:30:00');
+
+INSERT INTO tb_usuario (nome, login, email, senha, data_criacao)
+VALUES ('Cláudio Santos', 'claudio_santos', 'claudio.santos@hotmail.com', '$2a$10$aZf7xh6zf.2NPNNIuxGTzej3.YRVDSv9FE4hMUZYky0KhlkN0oZcq', '2023-02-21 10:30:00');
+
+INSERT INTO tb_usuario (nome, login, email, senha, data_criacao)
+VALUES ('Carla Rosa', 'carla_rosa', 'carla.rosa@yahoo.com', '$2a$10$aZf7xh6zf.2NPNNIuxGTzej3.YRVDSv9FE4hMUZYky0KhlkN0oZcq', '2023-02-21 10:30:00');
+
+INSERT INTO tb_usuario (nome, login, email, senha, data_criacao)
+VALUES ('Gabriel Fernando', 'gabriel_fernando', 'gabriel.fernando@gmail.com', '$2a$10$aZf7xh6zf.2NPNNIuxGTzej3.YRVDSv9FE4hMUZYky0KhlkN0oZcq', '2023-02-21 10:30:00');
+
+
+
+
